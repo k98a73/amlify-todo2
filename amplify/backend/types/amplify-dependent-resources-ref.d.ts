ceed617ad41a303f152e3a4981e1067b6ab6def5
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-10-16 13:43:44 +0900

    AmplifyAPIの追加

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyTodo2Gql": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
